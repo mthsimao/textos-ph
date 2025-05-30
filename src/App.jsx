c3e9730d1d10c84ec0e4d89ced7128277002dec6
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2025-05-30 19:02:54 -0300

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div className="min-h-screen bg-white bg-img">
 
       <div className="text-title mb-5 p-5">
-        <h1 className="text-3xl text-center font-medium pb-2 text-white md:text-black">Textos</h1>
+        <h1 className="text-3xl text-center pb-2 text-white font-medium md:text-black">Textos</h1>
         <p className="font-medium italic text-center text-gray-400">Poemas criados por: <a href="https://www.instagram.com/_ph.pinheiro_/" target="_blank" className="underline">Pedro Henrique</a></p>
       </div>
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,14 @@ import { Link } from 'lucide-react';
 function Footer() {
   return (
     <>
-      <footer className="border-black/20 border-t p-8 mt-10 bg-white/75">
-        <div className="flex justify-center items-center">
-          <h2 className='flex flex-row items-center italic font-medium'>
+      <footer className="p-4 ">
+        <div className="flex justify-center items-center bg-white/85 p-4 rounded-full">
+          <h2 className='flex flex-row italic font-medium'>
             Feito por:
             <a
               href="https://matsimao.vercel.app"
               target="_blank"
-              className="text-blue-700 underline pl-2 flex flex-row gap-1"
+              className="text-blue-700 underline pl-2 flex flex-row items-center gap-1"
             >
               Matheus Simão 
               <Link className='h-4 w-4'/>
